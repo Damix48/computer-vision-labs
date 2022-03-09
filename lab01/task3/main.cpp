c127a1961a1af72b03add18dc7f1965a9203e86a
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-03-09 19:09:02 +0100

    show image at the same time

--- lab01/task3/main.cpp
+++ lab01/task3/main.cpp
@@ -2,8 +2,6 @@
 #include <opencv2/highgui.hpp>
 
 int main(int argc, char** argv) {
-  cv::namedWindow("Example 1");
-
   int height = 256;
   int width = 256;
   cv::Mat img1 = cv::Mat(height, width, CV_8UC1);
@@ -15,17 +13,14 @@ int main(int argc, char** argv) {
     }
   }
 
-  cv::imshow("Example 1", img1);
-
-  cv::waitKey(0);
-
   for (int i = 0; i < height - 1; i++) {
     for (int j = 0; j < width - 1; j++) {
       img2.at<unsigned char>(j, i) = static_cast<unsigned char>(i);
     }
   }
 
-  cv::imshow("Example 1", img2);
+  cv::imshow("Example 1", img1);
+  cv::imshow("Example 2", img2);
 
   char key = cv::waitKey(0);
   std::cout << key << std::endl;
