a88cd20e97c8468ca556e5ecc495025e22c8fd72
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-03-09 20:46:13 +0100

    add lab01 task 3

--- lab01/task3/main.cpp
+++ lab01/task3/main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 #include <opencv2/highgui.hpp>
 
-void checkboard(cv::Mat& image, int size);
+void create_chessboard(cv::Mat& image, int size);
 
 int main(int argc, char** argv) {
-  int height = 256;
-  int width = 256;
-  cv::Mat img1 = cv::Mat(height, width, CV_8UC1);
-  cv::Mat img2 = cv::Mat(height, width, CV_8UC1);
+  int size = 256;
 
-  for (int i = 0; i < height - 1; i++) {
-    for (int j = 0; j < width - 1; j++) {
+  cv::Mat img1 = cv::Mat(size, size, CV_8UC1);
+  cv::Mat img2 = cv::Mat(size, size, CV_8UC1);
+
+  size = 300;
+
+  cv::Mat img3 = cv::Mat(size, size, CV_8UC1);
+  cv::Mat img4 = cv::Mat(size, size, CV_8UC1);
+
+  for (int i = 0; i < img1.rows; i++) {
+    for (int j = 0; j < img1.cols; j++) {
       img1.at<unsigned char>(i, j) = static_cast<unsigned char>(i);
     }
   }
 
-  for (int i = 0; i < height - 1; i++) {
-    for (int j = 0; j < width - 1; j++) {
+  for (int i = 0; i < img2.rows; i++) {
+    for (int j = 0; j < img2.cols; j++) {
       img2.at<unsigned char>(j, i) = static_cast<unsigned char>(i);
     }
   }
 
+  create_chessboard(img3, 20);
+  create_chessboard(img4, 30);
+
   cv::imshow("Example 1", img1);
   cv::imshow("Example 2", img2);
+  cv::imshow("Example 3", img3);
+  cv::imshow("Example 4", img4);
 
   char key = cv::waitKey(0);
   std::cout << key << std::endl;
@@ -30,5 +40,34 @@ int main(int argc, char** argv) {
   return 0;
 }
 
-void checkboard(cv::Mat& image, int size) {
+void create_chessboard(cv::Mat& image, int size) {
+  bool alt_row = false;
+
+  for (int i = 0; i < image.rows; i++) {
+    bool alt_col = false;
+
+    bool changerow = ((i % size) == 0);
+
+    if (changerow) {
+      alt_row = !alt_row;
+    }
+
+    if (alt_row) {
+      alt_col = true;
+    }
+
+    for (int j = 0; j < image.cols; j++) {
+      bool changecol = (j % size) == 0;
+
+      if (changecol) {
+        alt_col = !alt_col;
+      }
+
+      if (alt_col) {
+        image.at<unsigned char>(i, j) = static_cast<unsigned char>(255);
+      } else {
+        image.at<unsigned char>(i, j) = static_cast<unsigned char>(0);
+      }
+    }
+  }
 }
