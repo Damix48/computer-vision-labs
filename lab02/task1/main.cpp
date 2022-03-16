392b354664281fad043d758347a3ca91ce2057ac
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-03-16 14:49:09 +0100

    add lab02 and lab02/task1

---
+++ lab02/task1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+int main(int argc, char** argv) {
+  cv::Mat img = cv::imread(argv[1]);
+  cv::Mat img_gray;
+
+  cv::imshow("Example 1", img);
+
+  cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
+
+  cv::imshow("Example 1 gray", img_gray);
+
+  cv::imwrite(argv[2], img_gray);
+
+  int key = cv::waitKey(0);
+
+  return 0;
+}
