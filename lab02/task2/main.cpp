a561e35c06db7b22781aa22e66ba6b5a7e1d06b5
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-03-17 14:30:23 +0100

    add max filter (lab02/task2)

--- lab02/task2/main.cpp
+++ lab02/task2/main.cpp
@@ -8,9 +8,18 @@ int main(int argc, char** argv) {
   cv::Mat img = cv::imread(argv[1]);
   cv::Mat img_gray;
   cv::Mat img_min;
+  cv::Mat img_max;
 
   cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
-  min_filter(img_gray, img_min, 15);
+  min_filter(img_gray, img_min, 3);
+  max_filter(img_gray, img_max, 3);
+
+  cv::imshow("Original", img);
+  cv::imshow("Gray", img_gray);
+  cv::imshow("Min filter", img_min);
+  cv::imshow("Max filter", img_max);
+
+  cv::waitKey(0);
 
   return 0;
 }
--- lab02/task2/src/filters.cpp
+++ lab02/task2/src/filters.cpp
@@ -27,3 +27,28 @@ void min_filter(cv::Mat src, cv::Mat& dst, int kernel_size) {
     }
   }
 }
+
+void max_filter(cv::Mat src, cv::Mat& dst, int kernel_size) {
+  if (kernel_size % 2 == 0) {
+    return;
+  }
+
+  dst = cv::Mat::zeros(src.rows, src.cols, src.type());
+
+  for (int i = 0; i < dst.rows; i++) {
+    for (int j = 0; j < dst.cols; j++) {
+      u_char max = 0;
+      for (int k = -kernel_size / 2; k < kernel_size / 2 + 1; k++) {
+        for (int w = -kernel_size / 2; w < kernel_size / 2 + 1; w++) {
+          if ((i + k > 0) && (i + k < dst.rows) && (j + w > 0) && (j + k < dst.cols)) {
+            u_char curr = src.at<u_char>(i + k, j + w);
+            if (max < curr) {
+              max = curr;
+            }
+          }
+        }
+      }
+      dst.at<u_char>(i, j) = max;
+    }
+  }
+}
