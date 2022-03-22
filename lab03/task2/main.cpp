1096f68ea68b0dd8add634530e64dab664f1ed0e
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-03-22 17:36:26 +0100

    add lab03/task2

---
+++ lab03/task2/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <opencv2/highgui.hpp>
+
+void onMouse(int event, int x, int y, int, void* _img) {
+  cv::Mat* img = static_cast<cv::Mat*>(_img);
+
+  if (event == cv::EVENT_LBUTTONUP) {
+    cv::Vec3b pixel = img->at<cv::Vec3b>(y, x);
+    std::cout << "Color at ("
+              << x << ", " << y << "): ("
+              << (int)pixel[0] << ", "
+              << (int)pixel[1] << ", "
+              << (int)pixel[2] << ")"
+              << std::endl;
+  }
+}
+
+int main(int argc, char** argv) {
+  cv::Mat img = cv::imread(argv[1]);
+
+  cv::namedWindow("Image");
+  cv::imshow("Image", img);
+
+  cv::setMouseCallback("Image", onMouse, &img);
+
+  int key = cv::waitKey(0);
+
+  return 0;
+}
