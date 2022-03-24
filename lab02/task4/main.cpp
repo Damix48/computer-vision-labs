7f15060d36d1159ce4fe75c2c0e04147238db975
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-03-24 11:15:49 +0100

    add lab02/task4

--- lab02/task4/main.cpp
+++ lab02/task4/main.cpp
@@ -8,41 +8,40 @@ int main(int argc, char** argv) {
 
   cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
 
-  int hbins = 30, sbins = 32;
-  int histSize[] = {hbins, sbins};
-  // hue varies from 0 to 179, see cvtColor
-  float hranges[] = {0, 180};
-  // saturation varies from 0 (black-gray-white) to
-  // 255 (pure spectrum color)
-  float sranges[] = {0, 256};
-  const float* ranges[] = {hranges, sranges};
-  cv::MatND hist;
-  // we compute the histogram from the 0-th and 1-st channels
-  int channels[] = {0, 1};
-  calcHist(&img, 1, channels, cv::Mat(),  // do not use mask
-           hist, 2, histSize, ranges,
-           true,  // the histogram is uniform
-           false);
+  cv::Mat hist;
 
-  double maxVal = 0;
-  minMaxLoc(hist, 0, &maxVal, 0, 0);
-  int scale = 10;
-  cv::Mat histImg = cv::Mat::zeros(sbins * scale, hbins * 10, CV_8UC3);
-  for (int h = 0; h < hbins; h++)
-    for (int s = 0; s < sbins; s++) {
-      float binVal = hist.at<float>(h, s);
-      int intensity = cvRound(binVal * 255 / maxVal);
-      cv::rectangle(histImg, cv::Point(h * scale, s * scale),
-                    cv::Point((h + 1) * scale - 1, (s + 1) * scale - 1),
-                    cv::Scalar::all(intensity),
-                    -1);
-    }
+  int histSize = 256;
+  float range[] = {0, 256};
+  const float* ranges[] = {range};
+  int channels[] = {0};
 
+  cv::calcHist(&img_gray, 1, channels, cv::Mat(), hist, 1, &histSize, ranges, true, false);
+
+  int hist_w = 512, hist_h = 400;
+  int bin_w = cvRound((double)hist_w / histSize);
+
+  cv::Mat hist_img = cv::Mat(hist_h, hist_w, CV_8U);
+
+  cv::normalize(hist, hist, 0, hist_img.rows, cv::NORM_MINMAX, -1, cv::Mat());
+
+  for (int i = 0; i < histSize; i++) {
+    cv::rectangle(
+        hist_img,
+        cv::Point(bin_w * i, hist_h),
+        cv::Point(bin_w * (i + 1), hist_h - cvRound(hist.at<float>(i))),
+        cv::Scalar(255, 0, 0),
+        -1,
+        8);
+  }
+
+  cv::namedWindow("Original", cv::WINDOW_NORMAL);
   cv::imshow("Original", img);
-  cv::imshow("Gray", img_gray);
-  cv::imshow("Histogram", histImg);
 
-  // cv::imwrite(argv[2], img_gray);
+  cv::namedWindow("Gray", cv::WINDOW_NORMAL);
+  cv::imshow("Gray", img_gray);
+
+  cv::namedWindow("Histogram");
+  cv::imshow("Histogram", hist_img);
 
   int key = cv::waitKey(0);
 
