f9b5ddc453779ebfc18b5f9ceaea060e26fcf783
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-04-05 16:57:03 +0200

    add header guards

--- lab02/task2/include/filters.h
+++ lab02/task2/include/filters.h
@@ -1,4 +1,9 @@
+#ifndef FILTERS_H
+#define FILTERS_H
+
 #include <opencv2/highgui.hpp>
 
 void min_filter(cv::Mat src, cv::Mat& dst, int kernel_size);
 void max_filter(cv::Mat src, cv::Mat& dst, int kernel_size);
+
+#endif  // FILTERS_H
