1aa5dc527594855eedeb3703256336348d757737
Damiano Zanardo <damiano.zanardo@gmail.com>
2022-05-30 19:19:46 +0200

    completed lab08/task2

--- lab08/task2/main.cpp
+++ lab08/task2/main.cpp
@@ -1,118 +1,75 @@
 #include <iostream>
 #include <opencv2/calib3d.hpp>
-#include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <string>
 #include <vector>
 
-#include "panoramic_utils.h"
 #include "utils_opencv.h"
 
 int main(int argc, char** argv) {
   if (argc < 3) {
+    std::cout << "USAGE:" << std::endl
+              << "main [folder of checkerboard images] [image for testing remap]" << std::endl;
     return EXIT_FAILURE;
   }
 
   std::vector<std::string> files;
 
   cv::glob(argv[1], files);
-  double angle = atof(argv[2]);
-  double ratio = 3;
+
+  cv::Mat image = cv::imread(argv[2]);
+  cv::Mat imageRemapped;
 
   std::vector<cv::Mat> images;
-  std::vector<cv::Mat> colimages;
 
-  std::vector<std::vector<cv::KeyPoint>> keypoints;
-  std::vector<cv::Mat> descriptors;
+  cv::Size patternSize(6, 5);
+
+  std::vector<std::vector<cv::Vec3f>> objectPoints;
+  std::vector<std::vector<cv::Vec2f>> imagePoints;
+  cv::Mat cameraMatrix;
+  cv::Mat distCoeffs;
+  std::vector<cv::Mat> rvecs;
+  std::vector<cv::Mat> tvecs;
+  cv::Mat stdDeviationsIntrinsics;
+  cv::Mat stdDeviationsExtrinsics;
+  cv::Mat perViewErrors;
+
+  cv::Mat R;
+  cv::Mat newCameraMatrix;
+  cv::Mat map1;
+  cv::Mat map2;
 
   for (int i = 0; i < files.size(); i++) {
     cv::Mat img = cv::imread(files[i]);
-
-    colimages.push_back(img);
-
-    images.push_back(cylindricalProj(img, angle));
-
-    cv::Ptr<cv::SIFT> sift = cv::SIFT::create();
-
-    std::vector<cv::KeyPoint> keypoint;
-    cv::Mat descriptor;
-
-    sift->detectAndCompute(images[i], cv::noArray(), keypoint, descriptor);
-
-    descriptors.push_back(descriptor);
-    keypoints.push_back(keypoint);
+    images.push_back(img);
   }
 
-  std::vector<std::vector<cv::DMatch>> matches;
+  for (int i = 0; i < images.size(); i++) {
+    std::vector<cv::Vec2f> corners;
+    bool found = cv::findChessboardCorners(images[i], patternSize, corners);
+    if (found) {
+      imagePoints.push_back(corners);
 
-  for (int i = 1; i < descriptors.size(); i++) {
-    cv::Ptr<cv::BFMatcher> matcher = cv::BFMatcher::create(cv::NORM_L2, true);
-
-    std::vector<cv::DMatch> match;
-    std::vector<cv::DMatch> goodMatch;
-
-    matcher->match(descriptors[i - 1], descriptors[i], match);
-
-    float minDistance = match[0].distance;
-
-    for (int j = 1; j < match.size(); j++) {
-      if (match[j].distance < minDistance) {
-        minDistance = match[j].distance;
+      std::vector<cv::Vec3f> points;
+      for (int j = 0; j < 5; j++) {
+        for (int k = 0; k < 6; k++) {
+          points.push_back(cv::Vec3f(j, k, 0));
+        }
       }
+      objectPoints.push_back(points);
     }
-
-    // std::cout << minDistance << std::endl;
-
-    for (int j = 0; j < match.size(); j++) {
-      if (match[j].distance < ratio * minDistance) {
-        goodMatch.push_back(match[j]);
-      }
-    }
-
-    matches.push_back(goodMatch);
   }
 
-  std::vector<cv::Mat> homographies;
+  cv::calibrateCamera(objectPoints, imagePoints, images[0].size(), cameraMatrix, distCoeffs, rvecs, tvecs);  //, stdDeviationsIntrinsics, stdDeviationsExtrinsics, perViewErrors);
 
-  for (int i = 0; i < matches.size(); i++) {
-    std::vector<cv::DMatch> match = matches[i];
+  cv::initUndistortRectifyMap(cameraMatrix, distCoeffs, R, newCameraMatrix, images[0].size(), CV_32FC1, map1, map2);
 
-    std::vector<cv::Point2f> points1;
-    std::vector<cv::Point2f> points2;
+  cv::remap(image, imageRemapped, map1, map2, cv::INTER_LINEAR);
 
-    // std::cout << match.size();
+  showImage("img", image);
+  showImage("img map", imageRemapped);
 
-    for (int j = 0; j < match.size(); j++) {
-      points1.push_back(keypoints[i][match[j].queryIdx].pt);
-      points2.push_back(keypoints[i + 1][match[j].trainIdx].pt);
-    }
-
-    // std::cout << points1.size() << " " << points2.size() << std::endl;
-
-    homographies.push_back(cv::findHomography(points1, points2, cv::RANSAC));
-    // std::cout << std::endl
-    //           << homographies[i]
-    //           << std::endl;
-  }
-
-  for (int i = 0; i < homographies.size() - 1; i++) {
-    showImage("img1", images[i]);
-    showImage("img2", images[i + 1]);
-    cv::Mat wrap, wrapinverso, result;
-    cv::warpPerspective(images[i], wrap, homographies[i], cv::Size(images[i + 1].cols + images[i].cols, images[i].rows));
-    cv::warpPerspective(images[i + 1], wrapinverso, homographies[i], cv::Size(images[i + 1].cols + images[i].cols, images[i].rows), cv::WARP_INVERSE_MAP);
-    showImage("wrap", wrap);
-    showImage("wrapinverso", wrapinverso);
-
-    cv::Mat half1(wrapinverso, cv::Rect(0, 0, images[i].cols, images[i].rows));
-    // cv::Mat half2(result, cv::Rect(images[i].cols, images[i].rows, images[i + 1].cols, images[i + 1].rows));
-
-    images[i].copyTo(half1);
-    showImage("result", wrapinverso);
-
-    cv::waitKey(0);
-  }
+  cv::waitKey(0);
 
   return 0;
 }
